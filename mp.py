a231529d07efd59d8ad3b9dc62f4d1d82b15e337
Chase <ChaseDevMac@protonmail.com>
2022-04-17 19:07:41 -0500

    feat: output the distance from receiver to reflector

--- mp.py
+++ mp.py
@@ -1,58 +1,64 @@
-from random import sample
 import numpy as np
+import argparse
 from scipy.io import wavfile
-import matplotlib.pyplot as plt
+import json
 
 SPEED_OF_SOUND = 340  # meters/sec
 
-#Code to create 10 second long white noise wav file (Tx / x_t)
-"""
-samplerate, data = wavfile.read("white_noise.wav")
-print(samplerate)
-new_data = data[4410:8820]
-repeat_data = new_data
-for i in range(100):
-    repeat_data = np.concatenate((repeat_data, new_data), axis=0)
+parser = argparse.ArgumentParser(description="""Obtain the distance between a
+                                 receiver and a reflector based on the channel
+                                 impulse response""")
+parser.add_argument("-r", metavar="--received-audio", type=str,
+                    help="name of audio singal/file at the receiver")
+parser.add_argument("-c", metavar="--distance", type=str,
+                    help="name of configuration file")
+parser.add_argument("-o", metavar="--output", type=str,
+                    help="name of output file")
+args = parser.parse_args()
 
-plt.plot(repeat_data)
-plt.legend()
-plt.show()
+received_audio = args.r or "received_audio.wav"
+config_file = args.c or "signal_config.json"
+output_file = args.o or "distance.txt"
 
-wavfile.write("new_white_noise.wav", samplerate, repeat_data)
-"""
+# read the configuration generated from gen_
+with open(config_file, 'r') as file:
+    data = file.read()
 
-samplerate, data = wavfile.read("white_noise.wav")
-xt = data[4410:8820]
+config = json.loads(data)
 
-samplerate, data = wavfile.read("CybertoriumMono.wav")
-#print(samplerate)
-mid = int(len(data) / 1.5)
-yr = data[mid: mid + (samplerate // 10)]
-#print(len(yr))
+xt = np.array(config["training data"])
 
+samplerate, data = wavfile.read(received_audio)
+
+if (samplerate != config["samplerate"]):
+    print(f"""Inconsistent samplerates\n
+    received samplerate: {samplerate}\n
+    transmitted samplerate: {config['samplerate']}""")
+
+# grab data size equal to the transmitted data size
+mid = int(len(data) / 2)
+yr = data[mid: mid + config["window size"]]
+
+# convert time domain signals into frequency domain
 fxt = np.fft.fft(xt)
 fyr = np.fft.fft(yr)
 
-hrt = np.fft.ifft(np.divide(fyr, fxt))
+# channel impulse response
+cir = np.fft.ifft(np.divide(fyr, fxt))
 
-#plt.plot(hrt)
-#plt.show()
+# line of sight response
+los = np.argmax(np.absolute(cir))
 
-hrtmax = np.argmax(np.absolute(hrt))
-print(hrtmax)
+# Index gap computed for 1 foot distance between receiver and reflective surface
+index_padding = 45
 
-#Index gap computed for 1 foot distance between receiver and reflective surface
-indexDist = 45
+# reflected response
+reflected = np.argmax(np.absolute(cir[(los + index_padding): -1])) + index_padding + los
 
-hrt2ndmax = np.argmax(np.absolute(hrt[hrtmax + indexDist : -1])) + indexDist + hrtmax
-print(hrt2ndmax)
+change_in_samples = reflected - los
+change_in_time = change_in_samples * (1 / samplerate)
 
-gap = hrt2ndmax - hrtmax
-time = gap * (1 / samplerate)
+distance = SPEED_OF_SOUND * (change_in_time / 2)
 
-distance = SPEED_OF_SOUND * time / 2
-print(distance)
-
-
-
-
+output_file = open(output_file, "w")
+output_file.write(f"Distance from receiver and reflector: {distance} meters")
